66efa331a973a9c3166b610bddc4d399080d97cf
Ali <alidrl26@gmail.com>
2023-08-29 21:39:05 +0200

    navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,112 @@
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import Link from "next/link";
+import React from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Navbar = () => {
+  const currentUser = { displayName: "felix franko" };
+  return (
+    <>
+      <Disclosure as="nav" className="text-white fixed top-0 z-20 w-full">
+        <div className={`px-4 md:px-16 py-6 transition duration-500 `}>
+          <div className="relative flex items-center justify-between">
+            <Link href="/">
+              <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
+            </Link>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {currentUser && (
+                <h5 className="mr-2 capitalize">{currentUser?.displayName}</h5>
+              )}
+
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative">
+                <div>
+                  <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={currentUser?.photoURL || "/images/default-slate.png"}
+                      alt="user"
+                      loading="lazy"
+                      referrerPolicy="no-referrer"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/register"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Register
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/login"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Login
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/profile"
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Profile
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <span
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                          )}
+                          role="button"
+                          onClick={() => logOut()}
+                        >
+                          Log out
+                        </span>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </>
+  );
+};
+
+export default Navbar;
